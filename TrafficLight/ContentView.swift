8037058e5d9023ddee77c34e145475969eefae62
Carlos García Pérez <carlosziok@gmail.com>
2024-04-16 22:28:47 +0300

    Implement logic button

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redOpacity = 0.3
+    @State private var yellowOpacity = 0.3
+    @State private var greenOpacity = 0.3
+    @State private var currentLight = CurrentLight.red
+    @State private var lightIsOn = 1.0
+    @State private var lightIsOff = 0.3
+    @State private var isFirstTap = true
+    
     var body: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
@@ -16,34 +24,62 @@ struct ContentView: View {
                     .frame(width: 130, height: 130)
                     .foregroundStyle(.red)
                     .overlay(Circle().stroke(Color.white, lineWidth: 6))
-                    .opacity(0.5)
+                    .opacity(redOpacity)
                 Circle()
                     .frame(width: 130, height: 130)
                     .foregroundStyle(.orange)
                     .overlay(Circle().stroke(Color.white, lineWidth: 6))
-                    .opacity(0.5)
+                    .opacity(yellowOpacity)
                 Circle()
                     .frame(width: 130, height: 130)
                     .foregroundStyle(.green)
                     .overlay(Circle().stroke(Color.white, lineWidth: 6))
-                    .opacity(0.5)
+                    .opacity(greenOpacity)
                 Spacer()
-                Button(action: {}) {
-                    Text("START")
+                Button(action: {
+                    changeLight()
+                }) {
+                    Text(isFirstTap ? "START" : "NEXT")
                         .font(.title)
                         .foregroundColor(.white)
                         .frame(width: 160, height: 55)
-                        .background(.blue)
-                        .overlay(                            RoundedRectangle(cornerRadius: 10)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
                                 .stroke(Color.white, lineWidth: 6)
-                                )
+                        )
                 }
                 .padding(.bottom, 20)
             }
             .padding(.top, 20)
-            Spacer()
         }
     }
+    
+    private func changeLight() {
+        if isFirstTap {
+            isFirstTap = false
+        }
+        
+        switch currentLight {
+            case .red:
+                redOpacity = lightIsOn
+                greenOpacity = lightIsOff
+                currentLight = .yellow
+            case .yellow:
+                redOpacity = lightIsOff
+                yellowOpacity = lightIsOn
+                currentLight = .green
+            case .green:
+                yellowOpacity = lightIsOff
+                greenOpacity = lightIsOn
+                currentLight = .red
+        }
+    }
+}
+
+enum CurrentLight {
+    case red, yellow, green
 }
 #Preview {
     ContentView()
