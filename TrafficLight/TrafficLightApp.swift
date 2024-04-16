8d44f3f2e424ddcd116269c74580d807f4b6f0d3
Carlos García Pérez <carlosziok@gmail.com>
2024-04-16 20:20:24 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Carlos Garcia Perez on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
