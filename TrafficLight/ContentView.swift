5d1cfcff00702faa09142f0f1e47175e5a6e5e07
Carlos García Pérez <carlosziok@gmail.com>
2024-04-16 21:39:57 +0300

    Create Interface

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Circle()
+                    .frame(width: 130, height: 130)
+                    .foregroundStyle(.red)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 6))
+                    .opacity(0.5)
+                Circle()
+                    .frame(width: 130, height: 130)
+                    .foregroundStyle(.orange)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 6))
+                    .opacity(0.5)
+                Circle()
+                    .frame(width: 130, height: 130)
+                    .foregroundStyle(.green)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 6))
+                    .opacity(0.5)
+                Spacer()
+                Button(action: {}) {
+                    Text("START")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 160, height: 55)
+                        .background(.blue)
+                        .overlay(                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 6)
+                                )
+                }
+                .padding(.bottom, 20)
+            }
+            .padding(.top, 20)
+            Spacer()
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
